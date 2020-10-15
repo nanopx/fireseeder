b8630dd20d63dcdf92600bc1029d556d74679b4a
polidog <polidogs@gmail.com>
2020-10-16 00:31:24 +0900

    Supported local emulator

--- src/lib/hooks/useFirestore.ts
+++ src/lib/hooks/useFirestore.ts
@@ -6,15 +6,23 @@ import { importJsonSync } from '../utils/importJson'
 export interface FirebaseOptions {
   databaseUrl: string
   credential: string
+  emulator: string
 }
 
 export const useFirestore = (options: FirebaseOptions) => {
   const firestore = useMemo(() => {
     const credentialObj = importJsonSync(getUserPath(options.credential))
-
     const admin = getAdmin(options.databaseUrl, credentialObj)
+    const db = admin.firestore()
 
-    return admin.firestore()
+    if (options.emulator) {
+      db.settings({
+        host: options.emulator,
+        ssl: false
+      })
+    }
+
+    return db
   }, [options])
 
   return firestore
